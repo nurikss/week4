255c701c273024c0ba8c8f3a08f32b55f3597624
Nurislam Baratov <nurislambaratov2@gmail.com>
2023-10-28 16:47:42 +0600

    задача searchInsert но уже сделанный своими руками

--- searchInsert.js
+++ searchInsert.js
@@ -28,23 +28,67 @@
 // }
 // console.log(searchInsert([1, 2, 3, 4, 5, 6,  8], 7));
 
-function searchInsert(arr, num) {
-    let left = 0;
-    let right = arr.length - 1;
-    
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        
-        if (arr[mid] === num) {
-            return mid; // Число найдено, возвращаем индекс
-        } else if (arr[mid] < num) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return left; // Число не найдено, возвращаем индекс для вставки
-}
 
-console.log(searchInsert([1, 2, 3, 4, 5, 6, 7, ], 8));
+
+// function searchInsert(arr, num){
+//     let left = 0
+//     let right = arr.length - 1
+//     for(let midle; left <= right;){
+//         midle = Math.floor((right + left) / 2)
+
+//         if(arr[midle] === num){
+//             return midle
+//         }else if(arr[midle] < num){
+//             left = midle + 1
+//         }else{
+//             right = midle -1
+//         }
+//     }
+//     return left
+
+
+// }
+// console.log(searchInsert([1, 4, 7, 9, 15, 27], 8));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Это я уже сам сделал, после того как понял как ИИ сделал его, но я его сделал на цикле for не смотря на прежний код ИИ сделаный с циклом while
+
+// function searchInsert(arr, num){
+//     let left = 0
+//     let right = arr.length - 1
+//     for(let mid; left <= right;){
+//         mid = Math.floor((right + left) / 2)
+
+//         if(arr[mid] === num){
+//             return mid
+//         }else if(arr[mid] < num){
+//             left = mid + 1
+//         }else{
+//             right = mid - 1
+//         }
+//     }
+//     return left
+// }
+// console.log(searchInsert([1, 2, 3, 4, 5, 6, 7, 9], 8));
